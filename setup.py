a0a9ecb665357e551e26fe96fa2984a4bf2fa55a
klmahalakshmi0102 <lumbini.kandregula123@gmail.com>
2026-03-17 00:54:43 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Generative AI Medical Chatbot',
+    version='0.0.0',
+    author='Lumbini',
+    author_email='lumbini.kandregula123@gmail.com',
+    description='A medical chatbot that uses generative AI to provide information and support to users.',
+    packages=find_packages(),
+    install_requires=[]
+)
